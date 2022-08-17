1c0ba0fb3687eb535860af6145c1fdd689d8f2d4
PBillingsby <philbillingsby@gmail.com>
2022-08-17 12:20:31 -0400

    remove erroneous function call

--- verified.js
+++ verified.js
@@ -1,5 +1,5 @@
 export async function verified(state, action) {
-  const res = await SmartWeave.contracts.readContractState("ZGaL5DOMIYRw9YHZ_NZ2JoIjST1QwhiD6T1jePH381I").readState();
+  const res = await SmartWeave.contracts.readContractState("ZGaL5DOMIYRw9YHZ_NZ2JoIjST1QwhiD6T1jePH381I");
   const verifiedAddresses = res.state.votes
     .filter(vote => vote.status === 'passed')
     .reduce((a, { value }) => ({ ...a, [JSON.stringify(value)]: true }), {});
